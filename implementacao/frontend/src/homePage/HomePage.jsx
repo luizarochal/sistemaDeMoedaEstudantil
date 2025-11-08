4b9cf4475ee113f09ba721642cb66100f0f5d8fc
Leonardo de Souza Galvão <160603231+LeonardodeSouzaGalvao@users.noreply.github.com>
2025-11-08 09:52:48 -0300

    Feat: Implementação frontend

---
+++ implementacao/frontend/src/homePage/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "./coponentes/header.jsx";
+import PageInfo from "./coponentes/pageinfo.jsx";
+
+export default function HomePage() {
+  return (
+    <div className="bg-purple-400">
+      <Header />
+      <PageInfo />
+    </div>
+  );
+}
---
+++ implementacao/frontend/src/homePage/coponentes/Header.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header className="bg-yellow-400 text-white py-4 px-8 shadow-md flex justify-between items-center">
+      <div className="text-2xl font-bold flex items-center gap-2">
+       Sistema de Moeda Estudantil
+      </div>
+      <nav className="flex gap-4">
+        <a
+          href="#"
+          className="bg-white/20 px-4 py-2 rounded-full hover:bg-white/30 transition"
+        >
+          Início
+        </a>
+        <a
+          href="#"
+          className="bg-white/20 px-4 py-2 rounded-full hover:bg-white/30 transition"
+        >
+          Sobre
+        </a>
+        <a
+          href="#"
+          className="bg-white/20 px-4 py-2 rounded-full hover:bg-white/30 transition"
+        >
+          Contato
+        </a>
+      </nav>
+    </header>
+  );
+}
